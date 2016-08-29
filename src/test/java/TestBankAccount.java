5289cca62a323a515ede8c3593a7a0a887b84cc9
z003dfnj <kadir.onay@siemens.com>
2016-08-29 16:22:46 +0300

    2nd test

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -9,4 +9,11 @@ public class TestBankAccount {
     double amount = account.debit(5);
     Assert.assertEquals(5.0, amount);
   }
+  
+  @Test
+  public void testDebitWithSufficientFunds() {
+    BankAccount account = new BankAccount(10);
+    double amount = account.debit(15);
+    Assert.assertEquals(10.0, amount);
+  }
 }
